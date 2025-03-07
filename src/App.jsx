51a5b9a5bedb175ee09102b779a965276f3e67a2
usman siddiqi <usmansiddiqi2000@gmail.com>
2025-03-07 17:48:43 +0530

    chnaged

--- src/App.jsx
+++ src/App.jsx
@@ -6,26 +6,32 @@ import { Contact } from "./components/Contact"
 import Testimonials from "./components/Testimonials"
 import Footer from "./components/Footer"
 import ComingSoon from "./components/ComingSoon"
+import TagManager from "react-gtm-module"
 function App() {
- 
+
+  const gtmInd = {
+    gtmId: "GTM-TTTFQR6S",
+  };
+
+  TagManager.initialize(gtmInd);
 
   return (
-    
-    
-<div className="w-full overflow-hidden">
 
-  <Header/>
-<Projects/>
-<ComingSoon/>
-  <About/>
-<Testimonials/>
-<Contact/>
-<Footer/>
 
-</div>
-    
+    <div className="w-full overflow-hidden">
 
-    
+      <Header />
+      <Projects />
+      <ComingSoon />
+      <About />
+      <Testimonials />
+      <Contact />
+      <Footer />
+
+    </div>
+
+
+
   )
 }
 
